9c586bf5b1de1c464a860bf5bc8040538600711e
Raju1-Ahmed <robiussaniraju@gmail.com>
2022-06-10 00:41:15 +0600

    navLogo add and footer page add

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './Pages/Shared/Navbar/Navbar';
+import Footer from './Pages/Shared/Footer/Footer';
 
 function App() {
   return (
     <div className="App">
       <Navbar></Navbar>
+      <Footer></Footer>
     </div>
   );
 }
